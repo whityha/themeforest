d4772bb48de29a0c52687ddce702babf3b06638a
Pavel <2042_2042@mail.ru>
2023-04-05 11:28:42 +0300

    feat: add settings in project

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+// const CopyWebpackPlugin = require('copy-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+    entry: './src/index.tsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+        publicPath: '/',
+    },
+    plugins: [
+        new Dotenv(),
+        new CleanWebpackPlugin(),
+        // new CopyWebpackPlugin({
+        //     patterns: [
+        //         {
+        //             from: '**/*',
+        //             context: path.resolve(__dirname, 'src', 'assets'),
+        //             to: './assets',
+        //         },
+        //     ],
+        // }),
+        new HtmlWebpackPlugin({
+            title: 'FOREST',
+            template: './public/index.html',
+        }),
+        new NodePolyfillPlugin(),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.(ts|tsx)?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: ['file-loader'],
+            },
+            {
+                test: /\.(jpg|png|webp|ico)$/,
+                use: {
+                    loader: 'url-loader',
+                },
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['*', '.ts', '.tsx', '.js'],
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+        },
+    },
+};
