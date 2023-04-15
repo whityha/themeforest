62c6ac6cd8cb714ed7b6d3c0535113270d43c068
Pavel <2042_2042@mail.ru>
2023-04-15 08:56:12 +0300

    fix: change style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,7 @@ module.exports = {
         rules: [
             {
                 test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
+                use: 'css-loader',
             },
             {
                 test: /\.(ts|tsx)?$/,
